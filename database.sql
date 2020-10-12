3a7171940a52e0503d98f172365c814e4fb23548
John Hinderer <john.hinderer16@gmail.com>
2020-10-12 14:41:51 -0500

    pool db name changed

--- database.sql
+++ database.sql
@@ -1,10 +1,22 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "round" (
+    "id" SERIAL PRIMARY KEY,
+    "date" date,
+    "number_holes" INT NOT NULL,
+    "score_to_par" INT NOT NULL,
+    "putts" INT NOT NULL,
+    "approach_shots" INT NOT NULL,
+    "fairways_hit" INT NOT NULL,
+    "possible_fairways" INT NOT NULL
+);
+
+
+CREATE TABLE "user_round" (
+	"user_id" INT REFERENCES "user",
+	"round_id" INT REFERENCES "round"
 );
