25375469686a252c84aecf95363f3340fc186ef9
John Hinderer <john.hinderer16@gmail.com>
2020-10-16 12:30:52 -0500

    Edit button toggles stats to be displayed, must PUT

--- src/components/EditItem/EditItem.js
+++ src/components/EditItem/EditItem.js
@@ -5,8 +5,42 @@ import { connect } from 'react-redux';
 
 class EditItem extends Component {
 
-  editItem = (id) => {
+  state = {
+    edit: '',
+    date: '',
+    number_holes: '',
+    score_to_par: '',
+    putts: '',
+    approach_shots: '',
+    fairways_hit: '',
+    possible_fairways: '',
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      edit: false,
+      date: this.props.round.date.split('T', 1)[0],
+      number_holes: this.props.round.number_holes,
+      score_to_par: this.props.round.score_to_par,
+      putts: this.props.round.putts,
+      approach_shots: this.props.round.approach_shots,
+      fairways_hit: this.props.round.fairways_hit,
+      possible_fairways: this.props.round.possible_fairways,
+    })
+  }
+
+  editItem = () => {
     console.log('editing round');
+    this.setState({
+      edit: true,
+    });
+  }
+
+  confirmEdit = () => {
+    console.log('confirmEdit'); // this is where PUT dispatch will go
+    this.setState({
+      edit: false,
+    });
   }
 
   removeItem = (id) => {
@@ -17,23 +51,92 @@ class EditItem extends Component {
     });
   }
 
+  handleChangeFor = (event, propertyName) => {
+    console.log('handChangeFor hit');
+    this.setState({
+      ...this.state,
+      [propertyName]: Number(event.target.value)
+    });
+  }
+
+
+
+
+
   render() {
     //console.log('this.props', this.props.rounds);
+    console.log('this.state', this.state);
     return (
       <div>
-        {/* <p>thing</p> */}
 
-        <span>
-          {this.props.round.date.split('T', 1)[0]}
-          {this.props.round.number_holes}
-          {this.props.round.score_to_par}
-          {this.props.round.putts}
-          {this.props.round.approach_shots}
-          {this.props.round.fairways_hit}
-          {this.props.round.possible_fairways}
-        </span>
-        <button onClick={() => this.editItem(this.props.round.id)}>Edit</button>
-        <button onClick={() => { if (window.confirm('Are you sure you wish to delete this round?')) this.removeItem(this.props.round.id) } }>Delete</button>
+        {this.state.edit ?
+          <>
+            editing!
+            <form>
+              <span>Date Played:</span>
+              <input placeholder={this.state.date} onChange={(event) => this.handleChangeFor(event, 'date')} />
+
+              <div>
+                <button onClick={() => this.setState({ ...this.state, number_holes: (this.state.number_holes + 1) })}>+</button>
+                <span>Holes Played: {this.state.number_holes}</span>
+                <button onClick={() => this.setState({ ...this.state, number_holes: (this.state.number_holes - 1) })}>-</button>
+              </div>
+
+
+              <div>
+                <button onClick={() => this.setState({ ...this.state, score_to_par: (this.state.score_to_par + 1) })}>+</button>
+                <span>Strokes Over Par: {this.state.score_to_par}</span>
+                <button onClick={() => this.setState({ ...this.state, score_to_par: (this.state.score_to_par - 1) })}>-</button>
+              </div>
+
+
+              <div>
+                <button onClick={() => this.setState({ ...this.state, putts: (this.state.putts + 1) })}>+</button>
+                <span>Putts: {this.state.putts}</span>
+                <button onClick={() => this.setState({ ...this.state, putts: (this.state.putts - 1) })}>-</button>
+              </div>
+
+
+              <div>
+                <button onClick={() => this.setState({ ...this.state, approach_shots: (this.state.approach_shots + 1) })}>+</button>
+                <span>Approach Shots: {this.state.approach_shots}</span>
+                <button onClick={() => this.setState({ ...this.state, approach_shots: (this.state.approach_shots - 1) })}>-</button>
+              </div>
+
+
+              <div>
+                <button onClick={() => this.setState({ ...this.state, fairways_hit: (this.state.fairways_hit + 1) })}>+</button>
+                <span>Fairways Hit: {this.state.fairways_hit}</span>
+                <button onClick={() => this.setState({ ...this.state, fairways_hit: (this.state.fairways_hit - 1) })}>-</button>
+              </div>
+
+
+              <div>
+                <button onClick={() => this.setState({ ...this.state, possible_fairways: (this.state.possible_fairways + 1) })}>+</button>
+                <span>Possible Fairways: {this.state.possible_fairways}</span>
+                <button onClick={() => this.setState({ ...this.state, possible_fairways: (this.state.possible_fairways - 1) })}>-</button>
+              </div>
+
+
+              <button onClick={() => this.confirmEdit()}>Confirm</button>
+            </form>
+          </>
+          :
+          <> 
+            <span>
+              {this.props.round.date.split('T', 1)[0]}
+              {this.props.round.number_holes}
+              {this.props.round.score_to_par}
+              {this.props.round.putts}
+              {this.props.round.approach_shots}
+              {this.props.round.fairways_hit}
+              {this.props.round.possible_fairways}
+            </span>
+            <button onClick={() => this.editItem(this.props.round.id)}>Edit</button>
+            <button onClick={() => { if (window.confirm('Are you sure you wish to delete this round?')) 
+              this.removeItem(this.props.round.id)}}>Delete</button>
+          </>
+        }
 
       </div>
     )
