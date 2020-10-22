f26290705119fd5737c534dd7229119fee3228aa
John Hinderer <john.hinderer16@gmail.com>
2020-10-22 11:06:16 -0500

    formatting

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import './Dashboard.css';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
+//import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import ComboGraph from '../Graphs/ComboGraph';
 
